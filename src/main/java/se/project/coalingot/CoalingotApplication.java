49d64161d1593600e42ff8620d70f975188674b1
oat431 <oat431@gmail.com>
2022-01-14 10:37:42 +0700

    [🛠 setup] : backend project

---
+++ src/main/java/se/project/coalingot/CoalingotApplication.java
@@ -0,0 +1,13 @@
+package se.project.coalingot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoalingotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoalingotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/project/coalingot/CoalingotApplicationTests.java
@@ -0,0 +1,13 @@
+package se.project.coalingot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoalingotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
